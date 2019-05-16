491cb0a656ef588d25840c9c14cc7636328b6a27
Craig Eastwood <craigdavideastwood@gmail.com>
2019-05-16 23:33:18 +1000

    Added user edit ability with menu option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 	before_action :not_logged_in, only: [:new, :create]
+	before_action :setup_user, only: [:edit, :update]
+	before_action :require_same_user, only: [:edit, :update]
+
 	def new
 		@user = User.new
 	end
@@ -16,10 +19,15 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-
 	end
 
 	def update
+		if @user.update(user_params)
+			flash[:positive] = "Details successfully updated"
+			redirect_to root_path
+		else
+			render 'edit'
+		end
 	end
 
 	private
@@ -30,6 +38,17 @@ class UsersController < ApplicationController
 		def not_logged_in
 			if logged_in?
 				flash[:negative] = "You must be logged out to sign-up a new user"
+				redirect_to root_path
+			end
+		end
+
+		def setup_user
+			@user = User.find(params[:id])
+		end
+
+		def require_same_user
+			if !logged_in? || current_user != @user
+				flash[:negative] = "You are not authorised to perform that action."
 				redirect_to root_path
 			end
 		end
